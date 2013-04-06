ea969d6b2ca19f2e8e40f8d752a1691deda8b159
ilkin Balkanay <ilkinulas@gmail.com>
2013-04-06 12:16:51 +0300

    Logger güncellendi.

--- src/com/appsonfire/p2p/util/Logger.java
+++ src/com/appsonfire/p2p/util/Logger.java
@@ -4,29 +4,42 @@ import android.util.Log;
 
 public class Logger {
 	public static final String TAG = "p2p4Android";
+	public static int logLevel = Log.INFO;
+	
 
 	public static final void d(String s) {
-		Log.d(TAG, s);
+		if (Log.DEBUG >= logLevel) {
+			Log.d(TAG, s);
+		}
 	}
 
 	public static final void i(String s) {
-		Log.i(TAG, s);
+		if (Log.INFO >= logLevel) {
+			Log.i(TAG, s);
+		}
 	}
 
 	public static final void w(String s) {
-		Log.w(TAG, s);
+		if (Log.WARN >= logLevel) {
+			Log.w(TAG, s);
+		}
 	}
 
 	public static final void w(String s, Exception e) {
-		Log.w(TAG, s, e);
+		if (Log.WARN >= logLevel) {
+			Log.w(TAG, s, e);
+		}		
 	}
 
 	public static final void e(String s) {
-		Log.e(TAG, s);
+		if (Log.ERROR >= logLevel) {
+			Log.e(TAG, s);
+		}
 	}
 
 	public static final void e(String s, Exception e) {
-		Log.e(TAG, s, e);
+		if (Log.ERROR >= logLevel) {
+			Log.e(TAG, s, e);
+		}
 	}
-
 }
